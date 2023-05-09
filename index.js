0c7ead543da7dbac288b03a625e74871da5426a7
codyivy0 <codyivy0@gmail.com>
2023-05-09 00:44:14 -0500

    TODO:Complete state

---
+++ index.js
@@ -0,0 +1,47 @@
+const confirmBtn = document.getElementById("confirm-button");
+const inputFields = document.querySelectorAll(".input-fields input");
+const frontNumber = document.getElementById("front-number");
+const nameElement = document.getElementById("name");
+const expirationElement = document.getElementById("expiration");
+const backNumber = document.getElementById("back-number");
+
+
+confirmBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    inputFields.forEach((field) => {
+        if (field.value.trim() === "") {
+            const error = field.nextElementSibling;
+            error.textContent = "Can't be blank";
+            field.classList.add("red-border");
+        }
+        else if (field.id === "card-number" && !/^\d{16}$/.test(field.value.trim())) {
+            const error = field.nextElementSibling;
+            error.textContent = "Wrong format, numbers only. Must be 16 digits";
+            field.classList.add("red-border");
+        }
+        else {
+            const error = field.nextElementSibling;
+            error.textContent = String.fromCharCode(0xA0);
+            field.classList.remove("red-border");
+            
+            // Update card elements as input fields are completed
+            if (field.id === "card-number") {
+                const formattedNumber = field.value.trim().replace(/(\d{4})(?=\d)/g, '$1 ');
+                frontNumber.textContent = formattedNumber;
+            }
+            else if (field.id === "cardholder-name") {
+                nameElement.textContent = field.value.trim().toUpperCase();
+            }
+            else if (field.id === "exp-date-mm" || field.id === "exp-date-yy") {
+                const month = document.getElementById("exp-date-mm").value.trim();
+                const year = document.getElementById("exp-date-yy").value.trim();
+                const formattedDate = `${month.padStart(2, "0")}/${year.padStart(2, "0")}`;
+                document.getElementById("expiration").textContent = formattedDate;
+              }
+            else if (field.id === "cvc") {
+                backNumber.textContent = field.value.trim().replace();
+            }
+        }
+    });
+});
+
